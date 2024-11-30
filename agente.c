9bf6177d4d8303b3948207416adc87e593963628
Moises Alvarez <mfalvare@espol.edu.ec>
2024-11-30 13:54:51 -0500

    Update agente.c

--- agente.c
+++ agente.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+
+int keep_running = 1;
+
+// Manejo de señal para terminación
+void handle_sigint(int sig) {
+    keep_running = 0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -21,16 +29,34 @@ int main(int argc, char *argv[]) {
     printf("Monitoreando servicios: %s, %s\n", servicio1, servicio2);
     printf("Tiempo de actualización: %d segundos\n", tiempo_actualizacion);
 
-    // Comando para journalctl del primer servicio
-    char *args[] = {"journalctl", "-u", servicio1, "-n", "10", NULL};
+    // Configurar manejo de señal
+    signal(SIGINT, handle_sigint);
 
-    // Ejecuta el comando
-    printf("Ejecutando comando para servicio: %s\n", servicio1);
-    if (execvp(args[0], args) == -1) {
-        perror("Error ejecutando execvp");
-        return 1;
+    // Bucle de monitoreo
+    while (keep_running) {
+        // Comando para journalctl del primer servicio
+        char *args1[] = {"journalctl", "-u", servicio1, "-n", "10", NULL};
+        printf("Ejecutando comando para servicio: %s\n", servicio1);
+        if (fork() == 0) { // Proceso hijo
+            execvp(args1[0], args1);
+            perror("Error ejecutando execvp");
+            exit(1);
+        }
+        wait(NULL); // Espera a que termine el hijo
+
+        // Comando para journalctl del segundo servicio
+        char *args2[] = {"journalctl", "-u", servicio2, "-n", "10", NULL};
+        printf("Ejecutando comando para servicio: %s\n", servicio2);
+        if (fork() == 0) { // Proceso hijo
+            execvp(args2[0], args2);
+            perror("Error ejecutando execvp");
+            exit(1);
+        }
+        wait(NULL); // Espera a que termine el hijo
+
+        sleep(tiempo_actualizacion); // Espera antes de la siguiente actualización
     }
 
-    // Nota: El código nunca llegará aquí si execvp tiene éxito
+    printf("Monitoreo detenido.\n");
     return 0;
 }
