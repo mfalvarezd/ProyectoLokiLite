621f55a8297ca9ff4e3eee0c50536289df26f31d
Moises Alvarez <mfalvare@espol.edu.ec>
2024-12-15 02:14:51 -0500

    e

--- prueba_stress.c
+++ prueba_stress.c
@@ -16,24 +16,23 @@ int main() {
         // Proceso hijo: ejecutar el primer comando
         execlp("stress", "stress", "--cpu", "7", "--timeout", "15", (char *)NULL);
         perror("Error al ejecutar el primer comando");
-        return 1; // Salir si exec falla
+        return 1; 
     }
 
-    // Crear el segundo proceso hijo para el segundo comando
     pid2 = fork();
     if (pid2 < 0) {
         perror("Error al crear el segundo proceso");
         return 1;
     } else if (pid2 == 0) {
-        // Proceso hijo: ejecutar el segundo comando
+       
         execlp("stress", "stress", "--vm", "5", "--vm-bytes", "3G", "--timeout", "60", (char *)NULL);
         perror("Error al ejecutar el segundo comando");
-        return 1; // Salir si exec falla
+        return 1; 
     }
 
-    // Esperar a que ambos procesos hijos terminen
-    waitpid(pid1, NULL, 0);  // Espera a que termine el primer proceso
-    waitpid(pid2, NULL, 0);  // Espera a que termine el segundo proceso
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);  
 
     return 0;
 }
